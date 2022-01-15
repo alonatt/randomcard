eb628bcb2b4fc4e844b1ca57018b3e1ea62c8eb8
alonatt <alondra08@ug.uchile.cl>
2022-01-15 01:43:13 +0000

    otravez

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector(".card").classList.add(pintarandom());
+  document.querySelector(".card span").innerHTML = numerorandom();
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let numerorandom = () => {
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexnumeros = Math.floor(Math.random() * numeros.length);
+  return numeros[indexnumeros];
+};
+
+let pintarandom = () => {
+  let pintas = ["corazon", "diamante", "espada", "trebol"];
+  let indexpintas = Math.floor(Math.random() * pintas.length);
+  return pintas[indexpintas];
 };
